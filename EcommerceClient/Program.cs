913ab04f0bad319dbaf40d4e310f04854db95340
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2023-12-31 02:17:28 -0500

    Client: Agregar el ClientServices
    Configuración para consumir las api del Services

--- EcommerceClient/Program.cs
+++ EcommerceClient/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using EcommerceClient;
+using EcommerceSharedLibrary.Interfaces;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Inyección de dependencia
+builder.Services.AddScoped<IProduct, ClientServices>();
 
 await builder.Build().RunAsync();
---
+++ EcommerceClient/Services/ClientServices.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using EcommerceSharedLibrary.Interfaces;
+using EcommerceSharedLibrary.Models;
+using EcommerceSharedLibrary.Responses;
+
+namespace EcommerceClient;
+
+public class ClientServices(HttpClient httpClient) : IProduct
+{
+    private const string BaseUrl = "api/product";
+
+    # region Configuraciones para Serializar y Deserializar modelos/string
+
+    // Para serializar el modelo, el parametro es generico para no limitarlo y devuelve un string.
+    private static string serializedObj(object modelObject) 
+                => JsonSerializer.Serialize(modelObject,JsonOptions());
+    // Para deserializar un string, pasamos el json y devolvemos un objeto generico
+    private static T DeserializeJsonString<T>(string jsonString)
+                => JsonSerializer.Deserialize<T>(jsonString, JsonOptions())!;
+    private static StringContent GenerateStringContext(string serializedObj) 
+                => new(serializedObj, Encoding.UTF8,"application/jason");
+    // Para deserializar un string, pasamos el json y devolvemos un objeto generico en lista
+    private static IList<T> DeserializeJsonStringList<T>(string jsonString)
+                => JsonSerializer.Deserialize<IList<T>>(jsonString, JsonOptions())!;
+    private static JsonSerializerOptions JsonOptions()
+    {
+        return new JsonSerializerOptions{
+            AllowTrailingCommas = true,//omitir comas al final del json
+            PropertyNameCaseInsensitive = true, // no distinguir entre mayusculas y minuscula
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase, // los nombres de las politicas de propiedad en CamelCase
+            UnmappedMemberHandling = JsonUnmappedMemberHandling.Skip // omitir el manejo de los miembros del mapeo
+        };
+    }
+    #endregion
+
+    public async Task<ServiceResponse> AddProduct(Product product)
+    {
+        var response = await httpClient.PostAsync(BaseUrl, GenerateStringContext(serializedObj(product)));
+        if (!response.IsSuccessStatusCode)
+            return new ServiceResponse(false, "Ocurrio un error. Intenttelo mas tarde...");
+        var apiResponse = await response.Content.ReadAsStringAsync();
+        return DeserializeJsonString<ServiceResponse>(apiResponse);
+    }
+
+    public async Task<List<Product>> GetAllProducts(bool featuredProducts)
+    {
+        var response = await httpClient.GetAsync($"{BaseUrl}?featured={featuredProducts}");
+        if(!response.IsSuccessStatusCode) return null!;
+        var result = await response.Content.ReadAsStringAsync();
+        return [.. DeserializeJsonStringList<Product>(result)];
+    }
+}
